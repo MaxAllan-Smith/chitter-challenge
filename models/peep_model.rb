5a7ca9a7a1187b933d8ce97fd69fda07ebcfbf6c
Max Allan-Smith <Maxibon_1195@hotmail.com>
2023-02-10 11:26:54 +0000

    Created model classes

---
+++ models/peep_model.rb
@@ -0,0 +1,3 @@
+class PeepModel
+  attr_accessor :id, :title, :content, :time_created, :user_id  
+end
---
+++ models/user_model.rb
@@ -0,0 +1,3 @@
+class UserModel
+  attr_accessor :id, :username, :password, :first_name, :last_name, :email_address, :date_created
+end
